e566929e59d1f911dbd8e79c3de1918d512aa1cd
Administrator <rchandru.rajendran@gmail.com>
2022-12-23 14:40:51 +0530

    first commit

---
+++ CustomerAPI/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using CustomerAPI.Models;
+using CustomerAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerService _service;
+
+        public CustomerController(CustomerService service)
+        {
+            _service = service;
+        }
+        // GET: api/<CustomerController>
+       /* [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return _service.GetAll();
+        }*/
+
+
+        [HttpGet("getCustomerDetails")]
+        public Customer Get(string email)
+        {
+            return _service.get(email);
+        }
+
+        [HttpPost, Route("createCustomer")]
+        public async Task<ActionResult<CustomerCreationStatus>> Post([FromBody] Customer order)
+        {
+            CustomerCreationStatus customer = _service.Add(order);
+            if (customer != null)
+            {
+                return customer;
+            }
+            return BadRequest("Couldnt Add");
+        }
+
+    }
+}
---
+++ CustomerAPI/Services/CustomerService.cs
@@ -0,0 +1,69 @@
+﻿using CustomerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.Services
+{
+    public class CustomerService
+    {
+        private readonly CustomerContext _context;
+
+        public CustomerService(CustomerContext context)
+        {
+            _context = context;
+        }
+        public CustomerCreationStatus Add(Customer customer)
+        {
+            CustomerCreationStatus customerCreationStatus = new CustomerCreationStatus();
+            try
+            {
+                bool flag;   
+                // _context.SaveChanges();  
+                Customer customer1 = _context.customers.FirstOrDefault(p => p.Email == customer.Email);
+                if (customer1 != null) 
+                {
+                    customerCreationStatus.CustomerEmail = customer.Email;
+                    customerCreationStatus.Message = "Already exists";
+                    _context.creationStatuses.Add(customerCreationStatus);
+                    _context.SaveChanges();
+                }
+                else
+                {
+                    _context.customers.Add(customer);
+                    customerCreationStatus.CustomerEmail = customer.Email;
+                    customerCreationStatus.Message = "sucess";
+                    _context.creationStatuses.Add(customerCreationStatus);
+                    _context.SaveChanges();
+                }
+                return customerCreationStatus;
+            }
+            catch (DbUpdateConcurrencyException Dbce)
+            {
+                Console.WriteLine(Dbce.Message);
+            }
+            catch (DbUpdateException Dbe)
+            {
+                Console.WriteLine(Dbe.Message);
+            }
+            return null;
+        }
+
+       /* public ICollection<Customer> GetAll()
+        {
+            IList<Customer> customers1 = _context.customers.ToList();
+            if (customers1.Count > 0)
+                return customers1;
+            else
+                return null;
+        }
+*/
+
+        public Customer get(string email)
+        {
+            return _context.customers.FirstOrDefault(u => u.Email == email);
+        }
+    }
+}
